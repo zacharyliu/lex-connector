e73feedab0d54decfa68e22aa72a7a81840d8c61
Zachary Liu <zachary@zacharyliu.com>
2017-09-10 05:32:26 -0400

    log fix

--- application.py
+++ application.py
@@ -1,9 +1,14 @@
+import logging
 import tornado.web
 import tornado.wsgi
 
 from tornado.web import url
 from server import PingHandler, WSHandler
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)7s %(message)s",
+)
 application = tornado.wsgi.WSGIAdapter(tornado.web.Application([
     url(r"/ping", PingHandler),
     url(r"/(.*)", WSHandler),
